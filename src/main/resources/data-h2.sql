b9d8a17b436615787c5f4ec9c4db149adf963f29
seungtaekhong <hsk4991149@naver.com>
2023-02-07 11:14:23 +0900

    feat: mysql server connect, jasypt 사용

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,57 @@
+-- Test 실행 시 필요한 초기 데이터입니다. h2 문법에 맞춰져 있습니다.
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '홍고은','94.10.27.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '이하언','97.06.22.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '이대현','97.09.17.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '지화정','99.03.23.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '최주안','02.01.10.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '성기현','02.06.27.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '이찬주','03.09.30.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '안준범','95.10.12.','1234','default.png', '복덕방');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '박진선','92.10.23.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '소예은','96.07.27.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '신명환','97.12.26.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '김보경','01.09.03.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '노하영','03.04.21.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '양한서','03.09.29.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '신원일','03.11.13.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '오유미','04.05.20.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '이하은','97.08.22.','1234','default.png', '복덕복덕');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '탁민경','86.09.02.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '한울','92.03.09.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '홍선경','96.04.09.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '박예림','94.10.24.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '조대승','95.06.22.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '소예진','97.09.22.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '최찬영','03.10.03.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '김진환','00.12.25.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '소준섭','94.02.27.','1234','default.png', '복권');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '손혜영','86.07.24.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '정유정','94.01.02.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '이지혜','95.11.11.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '김영래','98.02.19.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '양지혜','99.04.24.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '홍정희','00.10.06.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '김승빈','02.10.26.','1234','default.png', '또복');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '윤지연','97.09.16.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '김지연','97.04.10.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '소명섭','99.06.23.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '정슬기','99.11.26.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '홍승택','00.03.27.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '오유환','01.11.01.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '김태윤','04.03.06.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '조민성','01.01.02.','1234','default.png', '복통');
+insert into USERS (ATTENDANCE_NUMBER, NAME, BIRTH, PASSWORD, FACE_IMAGE_URI, TEAM) values (0, '최민호','00.03.03.','1234','default.png', '복통');
+
+insert into POST (TITLE, DATE, CONTENT) values ('엎드려 복 받기-② 하나님 나라에서', '2023-01-29','믿음으로 구원받은 성도가 누릴 수 있는 복은 예수 그리스도 안에서 생명을 얻어 하나님의 나라의 백성이 되었다는 것입니다.
+하나님 나라는 마지막 때에 우리에게 나타날 천국이라는 공간적 개념과 지금 우리 삶 가운데 하나님의 통치와 임재로 드러나는 통치적 개념으로 구분되어집니다.
+성도는 지금을 하나님의 임재와 통치 가운데 살아가며 영원한 하나님의 나라에서의 삶을 소망하며 사는 사람들로서 자녀, 신부, 백성, 종 등의 정체성을 가지고 살아가는 사람들입니다.
+그러한 삶을 살아가는 성도는 마땅히 믿음을 가지고 겸손한 마음으로 하나님의 뜻과 계획에 순종하는 삶을 살게 되며 그 안에서 우리 삶의 기준이 하나님이 된다는 것을 알게 됩니다.
+청년부 지체들의 삶이 하나님의 나라 가운데 살며 하나님과 동행하는 복을 누리는 삶이 되길 소망합니다.');
+
+insert into POST (TITLE, DATE, CONTENT) values ('엎드려 복 받기 - ③ 가난한 마음, 부요한 삶', '2023-02-05','사람은 누구나 부요한 삶을 꿈꿉니다. 돈이 많아 부요하고 관계가 많아 부요하고 삶이 즐거워 부요한 삶을 살아가는 것을 싫어하거나 거부할 사람은 없을 것입니다.\r\n하지만 예수님께서는 우리에게 가난한 마음을 가지라고 하십니다. 우리가 일상에서 우리의 삶을 풍요롭게 채우기 위해 노력하는 것들을 내려놓고 예수님을 먼저 쫒으라 하십니다. \r\n그 이유는 우리가 스스로 우리의 삶을 행복하게 할 수 없고, 우리의 노력과 열심으로 우리 스스로의 구원을 이뤄낼 수 없기 때문입니다. 아무리 노력하고 잘살아간다고 해도, 심지어 교회활동을 아무리 열심히 한다고 해도 예수님이 없는 인생은 생명에 이를 수 없다는 것입니다.\r\n오직 가난한 마음으로 내 힘으로 구원을 얻을 수 없는 죽을 수밖에 없는 자임을 인정하는 자만이 회개하는 마음으로 예수님을 찾고 용서받고 구원받아 하나님 나라에, 영생에 이를 수 있습니다. \r\n이시간 나를 부요하게 하려고 애썼던 모든 것을 해로 여기고 오직 예수님을 내 마음의 중심에 모시는 복된 시간 되길 소망합니다.');
+
+insert into QUESTION (ORDERS, POST_ID, CONTENT) values (1, 1, '지금 내 삶을 이끌어가는 가장 큰 요소는 무엇인가요?(직업, 관심, 사람, 고민 등)');
+insert into QUESTION (ORDERS, POST_ID, CONTENT) values (2, 1, '삶에서 하나님의 인도하심과 이끄심을 경험한 적이 있나요?');
+
+insert into QUESTION (ORDERS, POST_ID, CONTENT) values (1, 2, '나는 행복하기 위해, 내 삶을 즐겁게 살기 위해 어떤 노력을 하고 있나요?');
+insert into QUESTION (ORDERS, POST_ID, CONTENT) values (2, 2, '나는 가난한 마음을 가지고 예수님을 바라고 있나요?');
