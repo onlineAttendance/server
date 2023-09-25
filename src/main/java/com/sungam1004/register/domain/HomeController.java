7ee73221b00069650c0056f17d51485e5d7af7ed
seungtaekhong <hsk4991149@naver.com>
2023-09-25 20:06:18 +0900

    feat: post test page

--- src/main/java/com/sungam1004/register/domain/HomeController.java
+++ src/main/java/com/sungam1004/register/domain/HomeController.java
@@ -1,6 +1,7 @@
 package com.sungam1004.register.domain;
 
 import com.sungam1004.register.global.manager.BuildTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @GetMapping("/")
@@ -20,6 +22,7 @@ public class HomeController {
     @PostMapping("/")
     @ResponseBody
     public String postHome(@RequestBody PostHome request) {
+        log.info("받은 데이터 = {}", request.getData());
         return "받은 데이터 = " + request.getData();
     }
 }
