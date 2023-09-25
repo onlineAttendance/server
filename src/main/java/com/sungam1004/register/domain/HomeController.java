55a6c960bf76461ff7ea0b4b88f79110a348cd2e
seungtaekhong <hsk4991149@naver.com>
2023-09-25 18:53:10 +0900

    feat: post test page

--- src/main/java/com/sungam1004/register/domain/HomeController.java
+++ src/main/java/com/sungam1004/register/domain/HomeController.java
@@ -4,6 +4,9 @@ import com.sungam1004.register.global.manager.BuildTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -14,4 +17,9 @@ public class HomeController {
         return "home";
     }
 
+    @PostMapping("/")
+    @ResponseBody
+    public String postHome(@RequestBody PostHome request) {
+        return "받은 데이터 = " + request.getData();
+    }
 }
---
+++ src/main/java/com/sungam1004/register/domain/PostHome.java
@@ -0,0 +1,13 @@
+package com.sungam1004.register.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class PostHome {
+    private String data;
+}
